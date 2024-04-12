decae908fab1c6f5b5af45d7f3886c4afc20f2db
Данил <danikkach666@gmail.com>
2024-04-12 19:17:23 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import { Suspense, lazy } from "react";
-// import css from './App.module.css';
+import "./App.css";
 
-import Loader from "./components/Loader/Loader";
-import Navigation from "./components/Navigation/Navigation";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
-const MovieDetailsPagee = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage")
-);
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
-);
-
-const App = () => {
+export default function App() {
   return (
-    <>
-      <Navigation />
-      <main>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
-            <Route path="/movies/:movieId" element={<MovieDetailsPagee />}>
-              <Route path="cast" element={<MovieCast />} />
-              <Route path="reviews" element={<MovieReviews />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage to="/" replace />} />
-          </Routes>
-        </Suspense>
-      </main>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
-};
-
-export default App;
+}
--- src/services/getDate.js
+++
@@ -1,4 +0,0 @@
-export const formatDate = (date) => {
-  const options = { month: "long", year: "numeric" };
-  return new Date(date).toLocaleDateString("en-US", options);
-};
